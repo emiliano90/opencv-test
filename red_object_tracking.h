cce8be76392cfdf41e23d71c418b040cde3fac4e
Toshiba <toshiba@toshiba-Satellite-A665.(none)>
2014-04-01 23:16:17 -0300

    reconocimiento de objeto rojo con cvblob

---
+++ red_object_tracking.h
@@ -0,0 +1,14 @@
+/*
+ * red_object_tracking.h
+ *
+ *  Created on: 01/04/2014
+ *      Author: toshiba
+ */
+
+#ifndef RED_OBJECT_TRACKING_H_
+#define RED_OBJECT_TRACKING_H_
+
+int mainRedObjectTracking();
+
+
+#endif /* RED_OBJECT_TRACKING_H_ */
